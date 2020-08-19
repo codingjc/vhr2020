69d53dc64cb6db1308f468f4598066fed859bc47
coding_jc <996186701@qq.com>
2020-08-19 23:04:12 +0800

    description:第一次推送

---
+++ src/main/java/cn/codingjc/vhr2020/Vhr2020Application.java
@@ -0,0 +1,13 @@
+package cn.codingjc.vhr2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vhr2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vhr2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codingjc/vhr2020/Vhr2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codingjc.vhr2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vhr2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
