adf0bb2fcd2b34a145c8efda7095d7e9f388f2a9
coding_jc <996186701@qq.com>
2020-10-06 09:27:58 +0800

    description:update README.md && SecurityConfig.java

--- src/main/java/cn/codingjc/vhr2020/config/SecurityConfig.java
+++ src/main/java/cn/codingjc/vhr2020/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -137,6 +138,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .csrf()
-                .disable();
+                .disable().exceptionHandling()
+                //没有认证时，在这里处理结果，不要重定向
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest req, HttpServletResponse resp, AuthenticationException e) throws IOException, ServletException {
+                resp.setContentType("application/json;charset=utf-8");
+                PrintWriter out = resp.getWriter();
+                RespBean respBean = RespBean.error("访问失败");
+                if (e instanceof InsufficientAuthenticationException) {
+                    respBean.setMsg("请求失败，请联系管理员");
+                }
+                String s = new ObjectMapper().writeValueAsString(respBean);
+                out.write(s);
+                out.flush();
+                out.close();
+            }
+        });
     }
 }
